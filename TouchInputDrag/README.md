0eee3cc16df7be98d21048365f1bf82d27bacec6
Dominik Gstoehl <dokgst@gmail.com>
2020-04-16 12:39:07 +0200

    Forgot to save
    my bad

--- TouchInputDrag/README.md
+++ TouchInputDrag/README.md
@@ -1,3 +1,29 @@
-# Input Touch Drag
-Component to get an Vector2 from a drag on a rect to use as horizontal/vertical input exposed by the public input
+# Touch Input Drag
+Component to get an Vector2 from a drag on a rect to use as horizontal/vertical input exposed by the public input.
+## Setup
 
+Create an empty gameobject and add the script, I named it TouchInputDrag.
+Add an image from UI objects, I named it DragZone(Canvas will be created).
+
+Assign Canvas to the public canvas in the editor, as seen in the picture.
+
+![EmptyGameObjectSetup](images/touchinputdrag1.jpg)
+
+Add a EventTrigger Component to the image.
+Add Events "OnDrag", "PointerUp" and "PointerDown".
+For the function select your gameobject TouchInputDrag and select the function in the TouchInputDrag component,
+they're named accordingly to the function.
+
+![EventTriggerSetup](images/touchinputdrag2.jpg)
+
+Now you can create a public TouchInputDrag in your CharacterController or wherever you need it exposed.
+
+```csharp
+public class CharacterController : MonoBehaviour {
+    public TouchInputDrag touchInput;
+
+    void Update(){
+        transform.Rotate(0, touchInput.input.x, 0, Space.Self);
+    }
+}
+```
--- TouchInputDrag/TouchInputDrag.cs
+++ TouchInputDrag/TouchInputDrag.cs
@@ -1,5 +1,40 @@
-namespace DefaultNamespace {
-    public class TouchInputDrag {
-        
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UnityUtitlities {
+    public class TouchInputDrag : MonoBehaviour {
+        private RectTransform rect;
+        public Vector2 input;
+        public bool isDown;
+        public Canvas canvas;
+        public float sensitivity=1;
+        private int count=-1;
+        private Touch touch;
+        void Start() {
+            rect = GetComponent<RectTransform>();
+        }
+
+        public void OnPointerDown(BaseEventData bed) {
+            PointerEventData ptr = (PointerEventData) bed;
+            if (!isDown) {
+                isDown = true;
+                count = ptr.pointerId;
+            }
+        }
+        public void OnPointerUp(BaseEventData bed) {
+            PointerEventData ptr = (PointerEventData) bed;
+            if (count==ptr.pointerId) {
+                isDown = false;
+                input =  Vector2.zero;
+            }
+        }
+
+        public void OnDrag(BaseEventData bed) {
+            PointerEventData ptr = (PointerEventData) bed;
+            if (count == ptr.pointerId) {
+                input = ptr.delta / canvas.scaleFactor * sensitivity;
+                print(input);
+            }
+        }
     }
 }
